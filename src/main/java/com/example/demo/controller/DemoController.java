861f6c0814fe276fc662ee5b3e789229ef82019a
donkihotel <106761177+donkihotel@users.noreply.github.com>
2023-06-26 17:38:03 +0900

    properties add

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DemoController {
+
+    @Value("${sample.value}")
+    private String sample;
+
+    @GetMapping
+    private String getSample() {
+        return sample;
+    }
+}
